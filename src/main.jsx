65493ea99156d55b3bc4b95b41002638b0f0adb6
saurabh <saurabh2113076@akgec.ac.in>
2024-07-23 17:31:29 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
 import './index.css';
 import authConfig from './authconfig/auth0-Config';
 
-ReactDOM.render(
-<Auth0Provider
-  domain={authConfig.domain}
-  clientId={authConfig.clientId}
-  redirectUri={authConfig.redirectUri}
->
-  <App />
-</Auth0Provider>,
-document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root")); // Updated line
+
+root.render(
+  <Auth0Provider
+    domain={authConfig.domain}
+    clientId={authConfig.clientId}
+    redirectUri={authConfig.redirectUri}
+  >
+    <App />
+  </Auth0Provider>
 );
