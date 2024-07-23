0164623c414eb4005b754702404743f25158836a
saurabh <saurabh2113076@akgec.ac.in>
2024-07-23 17:38:27 +0530

    some chages in json

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import
+import ReactDOM from 'react-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
 import './index.css';
 import authConfig from './authconfig/auth0-Config';
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Updated line
-
-root.render(
-  <Auth0Provider
-    domain={authConfig.domain}
-    clientId={authConfig.clientId}
-    redirectUri={authConfig.redirectUri}
-  >
-    <App />
-  </Auth0Provider>
+ReactDOM.render(
+<Auth0Provider
+  domain={authConfig.domain}
+  clientId={authConfig.clientId}
+  redirectUri={authConfig.redirectUri}
+>
+  <App />
+</Auth0Provider>,
+document.getElementById("root")
 );
